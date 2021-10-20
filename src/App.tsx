dfe0bce78edc13bc764766fbb761600a34cc9520
EdinaldoPSJ <edinaldopsj92@gmail.com>
2021-10-20 05:27:53 -0300

    Files for boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return <strong>oi</strong>;
+};
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return <main>Main</main>;
+};
+
+export default Home;
